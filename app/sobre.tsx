06b4f02eaad21e6c2cc2687b0a9603724efed7d0
Pedro Augusto Fernandes Francelino <pedro.francelino@aluno.senai.br>
2026-02-12 15:31:55 -0300

    Adicionadas as paginas, index e detalhes.

---
+++ app/sobre.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Sobre(){
+    const router = useRouter();
+    return(
+        <View style={styles.container}>
+            <Text style={styles.}
+        </View>
+    );
+
+}
